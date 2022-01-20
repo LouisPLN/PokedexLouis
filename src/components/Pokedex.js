ff414338c10d587a366057afa16b74f3f19f6514
LouisPLN <louispoulinpro@gmail.com>
2022-01-20 12:44:57 +0100

    Make Card and All Types Gradient

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,21 +1,33 @@
 import React from "react";
 import "../styles/pokedex.scss";
+import "../styles/type.scss";
 
 const Pokedex = ({ id, name, image, type }) => {
-  const style = `poke-card ${type}`;
+  const style = `poke-right ${type}`;
 
   const imageSrc = `/badges/${type}.png`;
 
   return (
-    <div className={style}>
-      <div className="poke-number">
+    <div className="poke-card">
+      <div className="poke-left">
+        <div className="poke-name">
+          <h3>{name}</h3>
+        </div>
+        <p>#0{id}</p>
+
+        <a href="#">Détails</a>
+      </div>
+      <div className={style}>
+        <img src={image} alt={name} />
+      </div>
+      {/* <div className="poke-number">
         <small>#0{id}</small>
       </div>
       <img src={image} alt={name} />
       <div className="poke-identity">
         <h3>{name}</h3>
         <img src={imageSrc} alt={type} />
-      </div>
+      </div> */}
     </div>
   );
 };
